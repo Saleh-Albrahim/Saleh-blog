33ab13afb1daa654d57438b04ba767ddbb5eb867
unknown <s.albrahim@outlook.com>
2021-04-23 17:10:04 +0300

    feat: Finish add blog backend

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,37 +1,64 @@
 const express = require('express');
 const router = express.Router();
 const Blog = require('../models/blog');
+const ErrorResponse = require('../utils/errorResponse');
 
-Blog.createCollection();
 router.get('/', async (req, res, next) => {
+  try {
+    const blogList = await Blog.find();
 
-  const blogList = await Blog.find();
+    const header = {
+      title: "Saleh blog",
+      body: "Welcome to the best blog in the world !"
+    };
 
-  const header = {
-    title: "Saleh blog",
-    body: "Welcome to the best blog in the world !"
-  };
-
-  res.render('homeView', { blogList, header });
+    res.render('homeView', { blogList, header });
+  }
+  catch (error) {
+    return next(new ErrorResponse(error.message, 500));
+  }
 });
 
 router.get('/blog/:id', (req, res, next) => {
-  res.render('index');
+  try {
+    res.render('index');
+  }
+  catch (error) {
+    return next(new ErrorResponse(error.message, 500));
+  }
 });
 
-router.get('/add-blog', (req, res, next) => {
-  console.log(req.body);
-  const header = {
-    title: "Add blog",
-    body: "Add new blog"
-  };
+router.get('/add', (req, res, next) => {
+  try {
+    const header = {
+      title: "Add blog",
+      body: "Add new blog"
+    };
 
-  res.render('addBlogView', { header });
+
+    res.render('addBlogView', { header });
+
+  }
+  catch (error) {
+    return next(new ErrorResponse(error.message, 500));
+  }
 });
 
-router.post('/add-blog', (req, res, next) => {
-  console.log(req.body);
-  res.render('index');
+router.post('/add', async (req, res, next) => {
+
+  try {
+    const { title, content, user } = req.body;
+    if (!title || !content || !user) {
+      return next(new ErrorResponse('Please fill all the fields', 400));
+    }
+
+    await Blog.create(req.body);
+
+    res.redirect('/');
+  }
+  catch (error) {
+    return next(new ErrorResponse(error.message, 500));
+  }
 });
 
 
