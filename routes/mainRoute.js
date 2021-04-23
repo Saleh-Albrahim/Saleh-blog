609186859f8556b33dda87ce6640976195ac7216
unknown <s.albrahim@outlook.com>
2021-04-23 17:45:58 +0300

    fix: fix typo in the model import

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Blog = require('../models/blog');
+const Blog = require('../models/Blog');
 const ErrorResponse = require('../utils/errorResponse');
 
 router.get('/', async (req, res, next) => {
